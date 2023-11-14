420847856f9214341f0ed7c4f00c083c283d7b90
Kimberley Bonix <kimberley.bonix@oclock.school>
2023-11-14 19:16:08 +0100

    :white_check_mark: bonus

--- src/router/Root/Root.tsx
+++ src/router/Root/Root.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Menu from '../../components/Menu/Menu';
 
 import './Root.scss';
 
 function Root() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="app">
       <Menu />
-
       <Outlet />
     </div>
   );
